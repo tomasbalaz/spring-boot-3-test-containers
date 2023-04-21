5c43265b648ed5335e76f10cf84974b2f6d2b43e
Tomas Balaz <balaztomas0@gmail.com>
2023-04-21 11:47:06 +0200

    Created spring-boot-3-test-containers application

---
+++ src/main/java/sk/balaz/springboot3testcontainers/SpringBoot3TestContainersApplication.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboot3testcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3TestContainersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBoot3TestContainersApplication.class, args);
+  }
+
+}
---
+++ src/test/java/sk/balaz/springboot3testcontainers/SpringBoot3TestContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboot3testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3TestContainersApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
