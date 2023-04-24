5c050830a543d68ba45be000af2bddec0f2c0984
Tomas Balaz <balaztomas0@gmail.com>
2023-04-24 15:54:29 +0200

    Introduced @TestPropertySource instead of use Junit5 @Testcontainers, @Container annotations

--- src/test/java/sk/balaz/springboot3testcontainers/CustomerRepositoryTest.java
+++ src/test/java/sk/balaz/springboot3testcontainers/CustomerRepositoryTest.java
@@ -4,37 +4,23 @@ package sk.balaz.springboot3testcontainers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-@Testcontainers
+@TestPropertySource(
+    properties = {
+        "spring.test.database.replace=none",
+        "spring.datasource.url=jdbc:tc:postgresql:15.2-alpine:///db"
+    }
+)
 class CustomerRepositoryTest {
-
-  @Container
-  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
 
   @Autowired
   CustomerRepository customerRepository;
-
-  @DynamicPropertySource
-  static void configureProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", postgres::getJdbcUrl);
-    registry.add("spring.datasource.username", postgres::getUsername);
-    registry.add("spring.datasource.password", postgres::getPassword);
-  }
 
   @BeforeEach
   void setUp() {
